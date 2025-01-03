bac94f7c12c8191f33c8bff1dd4e2ac7613c74a5
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2025-01-03 16:06:53 -0300

    Implementação da classe grafo.h

---
+++ Grafo.h
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+#include "No.h"
+#include "Aresta.h"
+
+using namespace std;
+
+class No;
+class Aresta;
+
+class Grafo
+{
+
+private:
+    int ordem;
+    int numArestas;
+    int totalArestas;
+    bool pesoNos;
+    bool pesoArestas;
+    bool direcionado;
+
+    No *primeiroNo;
+    No *ultimoNo;
+    No *removido;
+
+public:
+    Grafo(int ordem, int totalArestas, bool direcionado, bool pesoArestas, bool pesoNos);
+    ~Grafo();
+
+
+    void incOrdem();
+    void decOrdem();
+
+    int getNumArestas();
+    No *getPrimeiroNo();
+    No *getUltimoNo();
+    int getOrdem();
+    int getGrau();
+
+
+    void printGrau();
+    bool ehDigrafo();
+
+    void imprimeGrafo();
+
+    bool ehBipartido();
+    int nConexo();
+    bool ehDirecionado();
+    bool verticePonderado();
+    bool arestaPonderada();
+    bool ehCompleto();
+    bool ehArvore();
+    bool possuiArticulacao();
+    bool possuiPonte();
+    void carregaGrafo(const string &filename);
+    void novoGrafo(const string &configFile);
+};
